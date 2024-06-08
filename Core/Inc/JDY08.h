f280b14baaf881723ecc3f9115fdc7627fefa321
theom4 <154817034+theom4@users.noreply.github.com>
2024-06-08 19:02:43 +0300

    Add files via upload

---
+++ Core/Inc/JDY08.h
@@ -0,0 +1,45 @@
+
+#ifndef INC_JDY08_H_
+#define INC_JDY08_H_
+#include "stdbool.h"
+#include "stdint.h"
+#include "string.h"
+#include "stdio.h"
+#include "math.h"
+/*-------USER CONFIG AREA----------*/
+#include "stm32f7xx_hal.h"   /*Change this header according to the MCU used : "stm32[SERIES]xx.h" */
+#define RSSI_D0        (-56) /*The RSSI value at a distance of 1 meter, can be obtained by using the LightBlue app*/
+#define RING_BUFFER_SIZE 64  /*Size of the buffer used to store the data received from the JDY08 module via bluetooth*/
+#define TEMP_BUFFER_SIZE 32  /*Size of the buffer used to store temporaily the incoming data*/
+#define JDY08_UART       USART2
+/*Change this handle according to your peripheral*/
+extern UART_HandleTypeDef huart2;
+
+/*---------------------------------*/
+typedef struct
+{
+	uint8_t ring_buf[RING_BUFFER_SIZE];
+	uint8_t temp_buf[TEMP_BUFFER_SIZE];
+	uint16_t head;
+	uint16_t tail;
+	bool rx_unlocked;
+
+}RingBuf_TypeDef;
+bool buffer_full(void);
+void JDY08_init(void);
+
+bool buffer_empty(void);
+bool is_response(char*);
+void copy_after(char* str,char* dest_buf,uint16_t dest_len);
+bool copy_between(char* str1, char* str2, char* dest_buf,uint16_t len);
+bool get_after(char* string,char* dest_buf,uint16_t len);
+
+bool copy_until(char* str_delim,char* dest_buf,uint16_t len);
+uint16_t get_buff_size(void);
+void clear_ring_buf(void);
+void store_buffer(uint8_t* src_buf,uint16_t len);
+int32_t check_for_string(char* str);
+
+float rssi_get_n_factor(int16_t rssi_value,uint8_t dist);
+float rssi_get_distance(float n_factor,int16_t rssi_value);
+#endif /* INC_JDY08_H_ */
---
+++ Core/Src/JDY08.c
@@ -0,0 +1,268 @@
+#include "JDY08.h"
+
+RingBuf_TypeDef BT_RingBuf;
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
+{
+
+	if(huart ->Instance == JDY08_UART)/*Callback triggered for bluetooth port*/
+	{
+		store_buffer(BT_RingBuf.temp_buf,Size);
+		BT_RingBuf.rx_unlocked = true;
+	}
+
+}
+void JDY08_init(void)
+{
+	clear_ring_buf();
+	BT_RingBuf.rx_unlocked = true;
+}
+void clear_ring_buf(void)
+{
+    /*Disable the interrupts for preventing concurrent memory access*/
+	__HAL_UART_DISABLE_IT(&huart2,UART_IT_RXNE);
+	memset(BT_RingBuf.ring_buf,'0',RING_BUFFER_SIZE * sizeof(BT_RingBuf.ring_buf[0]));
+	BT_RingBuf.head = 0;
+	BT_RingBuf.tail = 0;
+	__HAL_UART_ENABLE_IT(&huart2,UART_IT_RXNE);
+}
+uint16_t get_buff_size(void)
+{
+	uint16_t elem_num = (BT_RingBuf.head - BT_RingBuf.tail) % RING_BUFFER_SIZE;
+	if(elem_num < 0)
+	{
+		elem_num += RING_BUFFER_SIZE;
+	}
+	return elem_num;
+}
+bool buffer_empty(void)
+{
+	if(BT_RingBuf.head == BT_RingBuf.tail)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool buffer_full(void)
+{
+	uint16_t next = (BT_RingBuf.head + 1) % RING_BUFFER_SIZE;
+	if(BT_RingBuf.tail == next)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+/* moves the tail to the first occurence of the specified string.
+ * If this is found,returns 1, otherwise 0
+ * USE IT ONLY WHEN SEARCHING FOR A SINGLE STRING WITHIN THE BUFFER, THE REMAINING STRINGS WILL BE "DELETED" */
+bool is_response(char* str)
+{
+	if(buffer_empty() == true)
+	{
+		return false;
+	}
+	uint16_t len = strlen(str);
+	uint16_t count = 0;
+
+	while(buffer_empty() == false)
+	{
+		while(BT_RingBuf.ring_buf[BT_RingBuf.tail] == str[count])
+		{
+			BT_RingBuf.tail = (BT_RingBuf.tail + 1) % RING_BUFFER_SIZE;
+			count++;
+		}
+	    if(count == len)
+	    {
+	    	return true;
+	    }
+	    else
+	    {
+	    	count = 0;
+	    	BT_RingBuf.tail = (BT_RingBuf.tail + 1) % RING_BUFFER_SIZE;
+	    }
+	}
+    return false;
+
+}
+/*brief Stores the received data via UART from the JDY module into the Ring Buffer*/
+void store_buffer(uint8_t* src_buf,uint16_t len)
+{
+	for(uint16_t i = 0;i< len ;i++)
+	{
+		BT_RingBuf.ring_buf[BT_RingBuf.head] = src_buf[i];
+		BT_RingBuf.head = (BT_RingBuf.head + 1) % RING_BUFFER_SIZE;
+	}
+}
+
+/*@brief copies the data from the Ring Buffer into the input buffer
+ * @param "len" is the number of elements to be copied
+ * @retval true if the number of elements copied == len otherwise false*/
+bool get_after(char* string,char* dest_buf,uint16_t len)
+{
+	while(!is_response(string))
+	{
+		//...
+	}
+	for(uint16_t idx = 0;idx < len ;idx++)
+	{
+		if(buffer_empty() == 1)
+		{
+			return false; // couldn't copy all the data
+		}
+		dest_buf[idx] = BT_RingBuf.ring_buf[BT_RingBuf.tail];
+		BT_RingBuf.tail = (BT_RingBuf.tail + 1) % RING_BUFFER_SIZE;
+	}
+	return true;
+}
+/* @brief checks for the occurence of a particular response in the buffer,
+ * @retval  num of chars until the given string, if the string is not found -1 is returned*/
+
+int32_t check_for_string(char* str)
+{
+	if(buffer_empty() == 1)
+	{
+		return -1;
+	}
+	uint16_t len = strlen(str);
+	uint16_t so_far = 0;
+	uint16_t ring_index = BT_RingBuf.tail;
+	//bool string_found = false;
+	uint16_t len_until_str = 0;/*no of bytes until the given string*/
+	while(so_far != len && ring_index != BT_RingBuf.head)
+	{
+		while((BT_RingBuf.ring_buf[ring_index] != str[so_far]) && (ring_index != BT_RingBuf.head))
+		{
+			len_until_str ++;
+			ring_index = (ring_index + 1) % RING_BUFFER_SIZE;
+		    if(ring_index == BT_RingBuf.head)
+		    {
+		        //string_found = false;
+		    	//return string_found; // we've searched through all the elements
+		    	return -1;
+		    }
+		}
+		if(ring_index == BT_RingBuf.head)return -1;
+
+		if(BT_RingBuf.ring_buf[ring_index] == str[so_far])
+		{
+			while(BT_RingBuf.ring_buf[ring_index] == str[so_far])
+			{
+
+				so_far ++;
+				ring_index = (ring_index + 1) % RING_BUFFER_SIZE;
+			}
+		}
+		if(so_far != len)
+		{
+
+			len_until_str = len_until_str + so_far;/*str not found here, hence update the len*/
+			so_far = 0;
+		}
+		else
+		{
+
+			//BT_RingBuf.tail = BT_RingBuf.head;/*Update the tail,the string was found*/
+			return len_until_str;
+			//string_found = true;
+			//return string_found;
+		}
+
+	}
+	//string_found = false;
+	//return string_found;
+	return -1;/*str not found*/
+}
+
+/*@brief Copies the data from the Ring Buffer into the input buffer between 2 specified strings
+ *@param "len" is the length of the input buffer
+ *@retval true if the length between the strings < length of the input buffer otherwise false*/
+bool copy_between(char* start_string, char* end_string, char* dest_buf,uint16_t len)
+{
+	while(!is_response(start_string))
+	{
+		//Wait here till the first string appears
+	}
+	uint16_t len_between = check_for_string(end_string);
+	if(len_between == -1) return false; //There is no end_string
+	if(len_between > len)
+	{
+		strncpy(dest_buf,(char*)(BT_RingBuf.ring_buf + BT_RingBuf.tail),len);
+		BT_RingBuf.tail = (BT_RingBuf.tail + len) % RING_BUFFER_SIZE;
+		return false; // Could not copy all the data
+	}
+	else
+	{
+		strncpy(dest_buf,(char*)(BT_RingBuf.ring_buf + BT_RingBuf.tail),len_between);
+		BT_RingBuf.tail = (BT_RingBuf.tail + len_between) % RING_BUFFER_SIZE;
+		return true;//all the data has been copied
+	}
+}
+
+/*Functions used for getting the distance between the phone and the module (a rough estimation)*/
+/*This function should be used at first in order to get the n factor*/
+float rssi_get_n_factor(int16_t rssi_value,uint8_t dist)
+{
+    float n_factor = (RSSI_D0 - rssi_value) / (10.0f * log10(dist));
+	return n_factor;
+}
+float rssi_get_distance(float n_factor,int16_t rssi_value)
+{
+	float distance = pow(10,(RSSI_D0 - rssi_value) / (n_factor * 10.0f));
+	return distance;
+}
+/*******************************************/
+/* @brief Copies the data from the Ring Buffer into the input buffer
+ * @param The specified string
+ * @param The input buffer
+ * @param The length of the input buffer
+ * %retval true if more than one element has been copied otherwise false*/
+bool copy_until(char* str_delim,char* dest_buf,uint16_t len)
+{
+	if(buffer_empty() == 1)
+	{
+		return false;
+	}
+	int32_t idx2 = 0;
+	//int32_t len2 = len;
+	int32_t len_until_str = check_for_string(str_delim);
+	//uint16_t start_tail = BT_RingBuf.tail;
+	if(len_until_str == -1) return false; /*string not found*/
+
+	for(int32_t idx = 0; idx < len_until_str; idx++)
+	{
+		dest_buf[idx2 ++] = BT_RingBuf.ring_buf[BT_RingBuf.tail];
+		BT_RingBuf.tail = (BT_RingBuf.tail + 1) % RING_BUFFER_SIZE;
+        if(idx2 == len && idx < len_until_str)
+        {
+        	BT_RingBuf.tail = (BT_RingBuf.tail + len_until_str - len) % RING_BUFFER_SIZE;
+        	return true;
+        }
+	}
+	/*no of elements copied is %len_until_str,update the tail*/
+	BT_RingBuf.tail = (BT_RingBuf.tail + strlen(str_delim)) % RING_BUFFER_SIZE;
+	return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
