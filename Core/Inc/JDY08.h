7d2bfe729de1136946aaf9b92f9eb499e3b699d1
theom4 <154817034+theom4@users.noreply.github.com>
2024-06-09 16:31:30 +0300

    Update JDY08.h

--- Core/Inc/JDY08.h
+++ Core/Inc/JDY08.h
@@ -7,6 +7,7 @@
 #include "stdio.h"
 #include "math.h"
 /*-------USER CONFIG AREA----------*/
+
 #include "stm32f7xx_hal.h"   /*Change this header according to the MCU used : "stm32[SERIES]xx.h" */
 #define RSSI_D0        (-56) /*The RSSI value at a distance of 1 meter, can be obtained by using the LightBlue app*/
 #define RING_BUFFER_SIZE 64  /*Size of the buffer used to store the data received from the JDY08 module via bluetooth*/
@@ -15,6 +16,7 @@
 /*Change this handle according to your peripheral*/
 extern UART_HandleTypeDef huart2;
 #define UART_HANDLE &huart2
+
 /*---------------------------------*/
 typedef struct
 {
@@ -39,8 +41,10 @@ uint16_t get_buff_size(void);
 void clear_ring_buf(void);
 void store_buffer(uint8_t* src_buf,uint16_t len);
 int32_t check_for_string(char* str);
+/*Use these functins for receiving and sending data to the module*/
 bool JDY08_Write(char* buf,uint16_t len);
 bool JDY08_Read(char* buf, uint16_t len);
+/*****************************************************************/
 float rssi_get_n_factor(int16_t rssi_value,uint8_t dist);
 float rssi_get_distance(float n_factor,int16_t rssi_value);
 #endif /* INC_JDY08_H_ */
