95aa193fd95e7b1dbe4815221eb62626479d3183
theom4 <154817034+theom4@users.noreply.github.com>
2024-06-10 18:58:14 +0300

    Update JDY08.c

--- Core/Src/JDY08.c
+++ Core/Src/JDY08.c
@@ -9,7 +9,7 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
 		store_buffer(BT_RingBuf.temp_buf,Size);
 		BT_RingBuf.rx_unlocked = true;
 		 HAL_UARTEx_ReceiveToIdle_DMA(UART_HANDLE,BT_RingBuf.ring_buf, RING_BUFFER_SIZE);
-                __HAL_DMA_DISABLE_IT(&DMA, DMA_IT_HT);
+                __HAL_DMA_DISABLE_IT(&DMA_HANDLE, DMA_IT_HT);
 	}
 
 }
